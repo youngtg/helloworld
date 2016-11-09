69691b954a20fe2d733b4d815ba1f9a4cfc5b846
Tulga <tulgamj@gmail.com>
2016-11-09 00:50:56 -0600

    fixing startup

--- HelloWorldApp/src/HelloWorldApp/Startup.cs
+++ HelloWorldApp/src/HelloWorldApp/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace HelloWorldApp
-{wada
+{
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
